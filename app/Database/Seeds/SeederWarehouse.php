58bba0a1d4a70f5b636bb2350cc8a999fb056150
Leandro Ventura <leandroventur19@gmail.com>
2025-06-06 17:53:14 -0300

    feat(seed): warehouses

---
+++ app/Database/Seeds/SeederWarehouse.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use CodeIgniter\I18n\Time;
+
+class SeederWarehouse extends Seeder
+{
+    public function run()
+    {
+        $warehouses = [
+            [
+                'name' => 'Depósito Principal',
+                'address' => 'Rua do Depósito, 100 - Galpão A',
+                'description' => 'Armazém principal para medicamentos gerais',
+                'created_at' => Time::now(),
+                'updated_at' => Time::now(),
+                'city' => 'Luanda',
+                'manager_id' => 1
+            ],
+            [
+                'name' => 'Câmara Fria',
+                'address' => 'Rua do Depósito, 100 - Galpão B',
+                'description' => 'Armazém refrigerado para medicamentos termolábeis',
+                'created_at' => Time::now(),
+                'updated_at' => Time::now(),
+                'manager_id' => 3
+            ],
+            [
+                'name' => 'Loja - Balcão',
+                'address' => 'Rua Principal, 50 - Loja',
+                'description' => 'Estoque da área de vendas',
+                'created_at' => Time::now(),
+                'updated_at' => Time::now(),
+                'city' => 'Luanda',
+                'manager_id' => 2
+            ]
+        ];
+        $this->db->table('warehouses')->insertBatch($warehouses);
+    }
+}
