5c37c595ff5fcdc200c1a94b133902e6fd8b6a15
Leandro Ventura <leandroventur19@gmail.com>
2025-06-19 19:11:41 -0300

    refactor(menu): change order list for stock and movements

--- app/Views/templates/dashboard.php
+++ app/Views/templates/dashboard.php
@@ -62,9 +62,14 @@
                     <!-- Artigos -->
                     <li class="pc-item pc-hasmenu">
                         <a href="#!" class="pc-link"><span class="pc-micon"><i class="fa fa-archive"></i></span><span class="pc-mtext">
-                                Artigos</span><span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
+                                Stock</span><span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
                         <ul class="pc-submenu">
-                            <li class="pc-item"><a class="pc-link" href="<?= base_url('dashboard/stock') ?>">Stock</a></li>
+                            <li class="pc-item"><a class="pc-link" href="<?= base_url('dashboard/stock') ?>">Artigos</a></li>
+
+                            <li class="pc-item"><a class="pc-link" href="<?= base_url('dashboard/stock/movements') ?>">Movimentos</a></li>
+
+                            <li class="pc-item"><a class="pc-link" href="<?= base_url('dashboard/levels') ?>">Controle</a></li>
+                            
                         </ul>
                     </li>
                     <!-- End artigos -->
